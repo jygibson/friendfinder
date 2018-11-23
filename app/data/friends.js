6c2107c7a151a9a8f8e6c289150ec16b52dd91b7
jygibson <jygibson922@gmail.com>
2018-11-23 08:17:47 -0500

    need javascript help

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,20 +1,87 @@
 //create the array to hold all the friends
 
 var friendsArray = [
-    {
+  {
       friendName: "Regina",
       friendPhoto: "https://thenypost.files.wordpress.com/2015/05/regina.jpg?quality=90&strip=all&w=1200",
-      q1: "4",
-      q2: "3",
-      q3: "1",
-      q4: "5",
-      q5: "1",
-      q6: "2",
-      q7: "1",
-      q8: "4",
-      q9: "4",
-      q10: "5",
-    }
+      scores: [
+        5,
+        4,
+        3,
+        2,
+        1,
+        2,
+        3,
+        4,
+        5,
+        2
+      ]
+    },
+    {
+      friendName: "Janice",
+      friendPhoto: "https://dz7u9q3vpd4eo.cloudfront.net/wp-content/legacy/posts/0e6029f1-0656-43ae-b0b8-d0d6569e113b.jpg",
+      scores: [
+        1,
+        1,
+        1,
+        2,
+        2,
+        2,
+        3,
+        4,
+        5,
+        3
+      ]
+    },
+    {
+      friendName: "Damien",
+      friendPhoto: "https://www.thesun.co.uk/wp-content/uploads/2016/07/nintchdbpict0002536073101.jpg?strip=all&w=1200&h=800&crop=1",
+      scores: [
+       2,
+       4,
+       3,
+       2,
+       1,
+       1,
+       3,
+       4,
+       3,
+        1
+      ]
+    },
+    {
+      friendName: "Gretchen",
+      friendPhoto: "https://i.gifer.com/PKso.gif",
+      scores: [
+       3,
+       5,
+       2,
+       3,
+       5,
+       5,
+       4,
+       2,
+       2,
+        1
+      ]
+    },
+    {
+      friendName: "Karen",
+      friendPhoto: "http://41.media.tumblr.com/8d21dbec71244473b2fa17530159c73e/tumblr_o1aozodP5X1sakbyto1_500.png",
+      scores: [
+       1,
+       1,
+       1,
+       4,
+       5,
+       4,
+       2,
+       3,
+       2,
+      3
+      ]
+    },
+    
   ];
   
   // export the array so other files can access it.
--- app/routing/apiroutes.js
+++ app/routing/apiroutes.js
@@ -1,20 +1,74 @@
 //first require the data from the friends.js file so that they can connect
 
 var friendData = require("../data/friends")
-
 //do the same module.exports route to call in the express router
 
-module.exports = function(app){
+module.exports = function (app) {
 
     //this will show the JSON data for the different friends
-app.get("/api/friends", function(req, res) {
-    res.json(friendData);
-  });
+    app.get("/api/friends", function (req, res) {
+        res.json(friendData);
+    });
 
-//using app.post we indicate what the server should do with received data
+    //using app.post we indicate what the server should do with received data
 
-app.post("data/friends", function (request, response){
- friendData.push(request.body);
-    response.json(true);
-})
+    app.post("/api/friends", function (request, response) {
+        //getting user's scores for comparison
+        console.log(request.body)
+        var scoresAll = [];
+        //a perfect match would have a score of 0
+        var bestMatchScore = 0;
+        var versusArray = [];
+        var sum = 0;
+        var newFriendScore = request.body.scores;
+
+
+        function scoreTotals() {
+            for (let a = 0; a < friendData.length; a++) {
+                for (let s = 0; s < newFriendScore.length; s++) {
+                    var friendScoresArray = [];
+                    friendScoresArray.push(friendData[a].scores)
+                    var matchScore = Math.abs(
+                        parseInt(friendScoresArray[s])- parseInt(newFriendScore[s])
+                    );
+                    console.log("new friend score " + newFriendScore[s]);
+                    console.log("the friendscoresarray " + friendScoresArray[s]);
+                    console.log("the match score is " + matchScore);
+                   var friendScoresArray = [];
+
+                   versusArray.push(matchScore);
+                };
+            };
+        };
+
+        function sumArray() {
+            console.log("the versus array " + versusArray)
+            for (let i = 0; i < versusArray.length; i++) {
+                sum += versusArray[i]
+            }
+            console.log("the sum is: " + sum)
+            scoresAll.push(sum);
+            console.log("the scores all array " + scoresAll)
+        }
+
+        for (let i = 0; i < friendData.length; i++) {
+            //another for loop within for comparing the newFriendScore
+            scoreTotals();
+            sumArray();
+            versusArray = [];
+        }
+
+        // console.log("the scores all array " + scoresAll)
+
+        for (let i = 0; i < scoresAll.length; i++) {
+            if (scoresAll[i] <= scoresAll[bestMatchScore]) {
+                bestMatchScore = i;
+            }
+        }
+
+        var bestie = friendData[bestMatchScore]
+        console.log("your new bestie" + bestie)
+
+        friendData.push(request.body);
+    })
 };
